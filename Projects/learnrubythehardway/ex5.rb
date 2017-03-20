5914e94b138cdb5367863373a45ebba842d7f0b5
Paul Kassar <your email>
2017-03-20 14:31:12 +0000

    10 first

---
+++ Projects/learnrubythehardway/ex5.rb
@@ -0,0 +1,17 @@
+name = 'Paul Kassar'
+age = 26 #not a lie
+height = 185 #cm
+weight = 81 #kg
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "he's #{height} cm tall"
+puts "he's #{weight} kg's heavy"
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts " His teeth are usually #{teeth} depending on the coffee"
+
+#this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
