d959716dda54c3bdb00b0ecf3048c333dedeb16a
Paul Kassar <paulkassar@gmail.com>
2017-03-22 11:41:57 +0000

    Add files via upload

---
+++ Projects/learnrubythehardway/ex21.rb
@@ -0,0 +1,19 @@
+def add(a,b)
+  puts "adding #{a} + #{b}"
+  return a + b
+end
+
+def subtract(a,b)
+  puts "subtracting #{a} - #{b}"
+  return a - b
+end
+
+def multiply(a,b)
+  puts "multiplying #{a} + #{b}"
+  return a * b
+end
+
+def divide(a,b)
+  puts "dividing #{a} + #{b}"
+  return a / b
+end
